dfd07b687ba2a7302a6b5158343eab3d97bcadd4
jardelgoncalves <jardelgoncalves1996@gmail.com>
2022-02-06 20:39:42 -0300

    refactor: add tag as a arg

--- app/lib/get-html.ts
+++ app/lib/get-html.ts
@@ -3,6 +3,7 @@ import { base64Encode } from "./base64-encode";
 import bg from '~/assets/bg.png'
 import logo from '~/assets/logo.png'
 import icon from '~/assets/icon.png'
+import { Tag } from "@prisma/client";
 
 function getCss() {
   const background = base64Encode(bg);
@@ -152,7 +153,7 @@ function getCss() {
   `
 }
 
-export function getHtml() {
+export function getHtml(tag: Tag) {
   const logoImage = base64Encode(logo)
   const iconImage = base64Encode(icon)
 
@@ -178,10 +179,10 @@ export function getHtml() {
                 <img src="${iconImage}" alt="Circulo com bordas roxas e um desenho semelhante a um diamante" />
               </div>
               <div class="card__image">
-                <img src="/images/jardel.png" alt="Jardel" />
+                <img src="${tag.avatar}" alt=${tag.name} />
                 <div class="card__image__content">
-                  <h2>Jardel Gonçalves</h2>
-                  <span>Front End Developer</span>
+                  <h2>${tag.name}</h2>
+                  <span>${tag.role}</span>
                   <div class="card__image__info">
                   <svg
                       xmlns="http://www.w3.org/2000/svg"
@@ -198,7 +199,7 @@ export function getHtml() {
                         fill="#fff"
                         d="M18.609 2.54a.66.66 0 00.7-1.117l-.7 1.118zm2.919.886a.66.66 0 00-1.04.81l1.04-.81zM9.493 1.44h5.273V.122H9.493V1.44zm12.524 7.252v5.273h1.319V8.692h-1.319zM2.241 13.965V8.692H.923v5.273H2.24zm7.252 7.252a7.251 7.251 0 01-7.252-7.252H.923a8.57 8.57 0 008.57 8.57v-1.319zm12.524-7.252a7.251 7.251 0 01-7.25 7.252v1.318a8.57 8.57 0 008.569-8.57h-1.319zM9.493.122a8.57 8.57 0 00-8.57 8.57H2.24A7.251 7.251 0 019.493 1.44V.122zm5.932 11.206a3.296 3.296 0 01-3.296 3.296v1.319a4.614 4.614 0 004.615-4.615h-1.319zm-3.296 3.296a3.296 3.296 0 01-3.296-3.296H7.515a4.614 4.614 0 004.614 4.615v-1.319zm-3.296-3.296a3.296 3.296 0 013.296-3.296V6.714a4.614 4.614 0 00-4.614 4.614h1.318zm3.296-3.296a3.296 3.296 0 013.296 3.296h1.319a4.614 4.614 0 00-4.615-4.614v1.318zm5.274-2.636v1.318c.728 0 1.318-.59 1.318-1.318h-1.318zm0 0h-1.318c0 .728.59 1.318 1.318 1.318V5.396zm0 0V4.077c-.728 0-1.318.59-1.318 1.319h1.318zm0 0h1.318c0-.729-.59-1.319-1.318-1.319v1.319zm-2.637 15.82H12.13v1.319h2.637v-1.319zm-2.637 0H9.493v1.319h2.636v-1.319zM14.766 1.44c1.413 0 2.729.404 3.843 1.1l.7-1.117A8.533 8.533 0 0014.765.122V1.44zm5.722 2.797a7.217 7.217 0 011.53 4.455h1.318a8.536 8.536 0 00-1.808-5.266l-1.04.81z"
                       ></path>
-                    </svg> jardel.sh
+                    </svg> ${tag.instagram}
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       width="20"
@@ -213,7 +214,7 @@ export function getHtml() {
                         strokeWidth="1.318"
                         d="M14.785 2.294A8.623 8.623 0 009.888.78c-4.733 0-8.57 3.778-8.57 8.438 0 7.232 8.57 12.657 8.57 12.657s8.57-5.425 8.57-12.657a8.31 8.31 0 00-1.536-4.822M13.56 9.22c0 1.997-1.645 3.616-3.673 3.616S6.215 11.216 6.215 9.22 7.86 5.603 9.888 5.603s3.673 1.619 3.673 3.616z"
                       ></path>
-                    </svg> Brazil
+                    </svg> ${tag.city}
                   </div>
                   <ul>
                     <li>Status: <span>Cursando</span></li>
