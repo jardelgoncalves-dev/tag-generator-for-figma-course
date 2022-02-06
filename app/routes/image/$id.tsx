3f6ee47c659964f3f7bc8e07003f7d31cfc385c9
jardelgoncalves <jardelgoncalves1996@gmail.com>
2022-02-06 20:36:55 -0300

    feat: move image path and dinamyc path

--- app/routes/image/$id.tsx
+++ app/routes/image/$id.tsx
@@ -1,9 +1,13 @@
-import { LoaderFunction } from "remix";
+import { LoaderFunction, redirect } from "remix";
 import { getHtml } from "~/lib/get-html";
 import { getScreenshot } from "~/lib/get-screenshot";
+import { db } from "~/utils/db.server";
 
-export const loader: LoaderFunction = async () => {
-  const html = getHtml();
+export const loader: LoaderFunction = async ({ params }) => {
+  const tag = await db.tag.findUnique({ where: { id: params.id } })
+  if (!tag) return redirect("/")
+
+  const html = getHtml(tag);
   const screenshot = await getScreenshot(html, 'jpeg', !!process.env.IS_DEV)
 
   return new Response(screenshot, {
