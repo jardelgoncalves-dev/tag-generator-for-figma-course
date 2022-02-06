4f35026067fb75a30279a85404969ee8b93dd795
jardelgoncalves <jardelgoncalves1996@gmail.com>
2022-02-06 20:38:29 -0300

    feat: add course link

--- app/components/NFTTag.tsx
+++ app/components/NFTTag.tsx
@@ -2,7 +2,10 @@ type NFTTagProps = { children: JSX.Element }
 
 export function NFTTag({ children }: NFTTagProps) {
   return (
-    <section className="nft">
+    <section className="nft column">
+      <div className="row center">
+          <a className="course-link" href="https://cursodefigma.com">cursodefigma.com</a>
+        </div>
       <div className="card">
         <div className="card__logo">
           <img src="/images/logo.png" alt="Logo do curso de figma" />
