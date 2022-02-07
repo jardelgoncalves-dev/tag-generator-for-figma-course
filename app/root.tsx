90c3480f7c5ab5914f5e0829042280686142149c
jardelgoncalves <jardelgoncalves1996@gmail.com>
2022-02-06 23:01:04 -0300

    feat: import global style

--- app/root.tsx
+++ app/root.tsx
@@ -1,16 +1,29 @@
 import {
   Links,
+  LinksFunction,
   LiveReload,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration
 } from "remix";
+
 import type { MetaFunction } from "remix";
 
+import globalStyle from "~/styles/global.css"
+
 export const meta: MetaFunction = () => {
-  return { title: "New Remix App" };
+  return { title: "Tag Generator" };
 };
+
+export const links: LinksFunction = () => {
+  return [
+    {
+      href: globalStyle,
+      rel: "stylesheet"
+    }
+  ]
+}
 
 export default function App() {
   return (
